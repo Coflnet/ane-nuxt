324403d4fd2096792dce8d4e08eb42d730abeb99
Tentamens <tentamenslol@gmail.com>
2025-04-29 09:41:29 -0500

    add translation to subscription

--- components/subscriptions/planCard.vue
+++ components/subscriptions/planCard.vue
@@ -1,32 +1,32 @@
 <template>
   <div :class="[
-    'rounded-lg overflow-hidden shadow-lg transition-all duration-200',
+    'rounded-lg overflow-hidden shadow-lg transition-all duration-200 max-h-[470px]',
     selectedPlan === plan.id ? 'ring-4 ring-indigo-500 transform scale-105' : 'hover:scale-105',
     currentPlan === plan.id ? 'border-2 border-indigo-400' : 'border border-gray-700',
   ]">
     <div class="bg-gray-800 p-6">
       <div class="flex justify-between items-center">
-        <h3 class="text-xl font-bold text-white">{{ plan.name }}</h3>
+        <h3 class="text-xl font-bold text-white">{{ $t(plan.name) }}</h3>
         <span v-if="currentPlan === plan.id"
           class="px-3 py-1 text-xs font-medium rounded-full bg-indigo-500 text-white">
-          Current
+          {{ $t('current') }}
         </span>
         <span v-else-if="plan.popular" class="px-3 py-1 text-xs font-medium rounded-full bg-indigo-500 text-white">
-          Popular
+          {{ $t('mostPopular') }}
         </span>
       </div>
       <div class="mt-4">
         <span class="text-4xl font-extrabold text-white">${{ plan.price }}</span>
-        <span class="text-gray-400 ml-1">/month</span>
+        <span class="text-gray-400 ml-1">{{ $t('month') }}</span>
       </div>
-      <p class="mt-4 text-gray-400">{{ plan.description }}</p>
+      <p class="mt-4 text-gray-400">{{ $t(plan.description) }}</p>
     </div>
     <div class="p-6 bg-gray-800 border-t border-gray-700">
-      <ul class="space-y-4">
+      <ul class="space-y-4 h-full">
         <li v-for="(feature, index) in plan.features" :key="index" class="flex items-start">
           <Icon name="tabler:check" v-if="feature.included" class="h-5 w-5 text-indigo-500 mr-2 flex-shrink-0 mt-0.5" />
           <Icon v-else name="tabler:x" class="h-5 w-5 text-gray-500 mr-2 flex-shrink-0 mt-0.5" />
-          <span :class="feature.included ? 'text-gray-300' : 'text-gray-500'">{{ feature.text }}</span>
+          <span :class="feature.included ? 'text-gray-300' : 'text-gray-500'">{{ $t(feature.text) }}</span>
         </li>
       </ul>
       <button @click="$emit('select-plan', plan.id)" :class="[
@@ -47,7 +47,7 @@
 
 <script setup lang="ts">
 import { planPrices, type Plan, type PlanId } from '~/types/SubscriptionConstants';
-
+const { t } = useI18n();
 
 
 const props = defineProps<{
@@ -56,25 +56,22 @@ const props = defineProps<{
   selectedPlan: PlanId | null;
 }>();
 
-const emit = defineEmits<{
+defineEmits<{
   (e: 'select-plan', planId: PlanId): void;
 }>();
 
 function getButtonText(): string {
-  if (props.currentPlan === props.plan.id) {
-    return 'Current Plan';
-  }
-  if (props.plan.available == false) {
-    return "Coming soon"
-  }
+  if (props.currentPlan === props.plan.id)
+    return t('currentPlan');
+
+  if (props.plan.available == false)
+    return t('comingSoon');
 
   if (!props.currentPlan)
-    return 'Select Plan';
+    return t('selectPlan');
 
-  if (planPrices[props.plan.id] ?? 0 > planPrices[props.currentPlan ?? '']! ?? 0) {
-    return 'Upgrade';
-  } else {
-    return 'Downgrade';
-  }
+  if (planPrices[props.plan.id] ?? 0 > planPrices[props.currentPlan ?? '']! ?? 0)
+    return t('upgrade');
+  return t('downgrade');
 }
 </script>
