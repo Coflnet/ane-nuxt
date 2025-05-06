771adeda838bbed5ff104582c513c289b1e7f068
Tentamens <tentamenslol@gmail.com>
2025-05-05 20:59:39 -0500

    update favicon

--- app.vue
+++ app.vue
@@ -3,12 +3,13 @@
     <NuxtLayout>
       <NuxtPage />
       <Notivue v-slot="item">
-        <Notification :item="item" :theme="slateTheme" />
+        <Notification
+          :item="item"
+          :theme="slateTheme"
+        />
       </Notivue>
     </NuxtLayout>
-
   </div>
-
 </template>
 
 <script setup>
@@ -16,25 +17,29 @@
 useHead({
   title: 'ANE - Advanced Notification Engine',
   meta: [
-    { name: 'description', content: 'Track auctions across marketplaces with custom filters and notifications' }
+    { name: 'description', content: 'Track auctions across marketplaces with custom filters and notifications' },
+  ],
+  link: [
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
   ],
 })
 
 onMounted(async () => {
   await nextTick()
-  if (import.meta.server) return;
+  if (import.meta.server) return
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('firebase-messaging-sw.js', { scope: '/' })
-      .then(reg => {
-        console.log(`Service Worker Registration (Scope: ${reg.scope})`);
+      .then((reg) => {
+        console.log(`Service Worker Registration (Scope: ${reg.scope})`)
       })
-      .catch(error => {
-        const msg = `Service Worker Error (${error})`;
-        console.error(msg);
-      });
-  } else {
-    console.warn('Service Worker not available');
+      .catch((error) => {
+        const msg = `Service Worker Error (${error})`
+        console.error(msg)
+      })
   }
-});
+  else {
+    console.warn('Service Worker not available')
+  }
+})
 </script>
